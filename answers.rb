87854f84e3523dad5d77f5b54d58c439918b3a98
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-09 14:58:04 -0700

    start

---
+++ answers.rb
@@ -0,0 +1,5 @@
+
+
+puts "I am the spooky Magic Eight Ball".colorize(:red)
+puts "ASK YOUR QUESTION, PEASANT".colorize(:red)
+question_input = gets.strip
---
+++ main.rb
@@ -0,0 +1,12 @@
+# - Menu
+#   - Question
+#     - Random
+#   - Exit
+class Main
+
+
+def Menu
+end
+
+def Question_output
+end
