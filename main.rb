151ad90fb92222697221821911fb129686e5a13c
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-09 15:12:02 -0700

    minor changes

--- main.rb
+++ main.rb
@@ -2,11 +2,25 @@
 #   - Question
 #     - Random
 #   - Exit
+require_relative 'answers'
+require 'colorize'
+
 class Main
 
+def initialize
+  answers = @answers
 
 def Menu
-end
-
-def Question_output
+  puts "==========================="
+  puts "======== Main Menu ========"
+  puts "==========================="
+  puts "1) Ask a question"
+  puts "2) Quit"
+  choice = gets.to_i
+  case choice
+    when 1
+      Answers.question_input
+    when 2
+      exit
+    end
 end
